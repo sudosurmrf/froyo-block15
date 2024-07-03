e30ccc7c097c2d14dfe78fd40d125f25cb2bc6eb
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2024-07-03 09:37:14 -0700

    created html page and js script, made initial object

---
+++ index.js
@@ -0,0 +1,22 @@
+let userFlavors = prompt(`what flavor's of icecream would you like? Please separate each flavor with a ","`);
+let flavorsArray = []
+// console.log(userFlavors)
+const inputToArray = () => {
+  flavorsArray = userFlavors.split(',');
+}
+
+inputToArray();
+// console.log(flavorsArray)
+
+const tableOrders = {
+  }
+const arrayToObject = () => {
+  for (let i = 0; i < flavorsArray.length; i+=1) {
+    let flavor = flavorsArray[i];
+    if (tableOrders[flavor]) {
+      tableOrders[flavor] += 1;
+    } else {
+      tableOrders[flavor] = 1;
+    }
+  }
+}
